1297d11edc7f5c591b4f1d801c468033761ac844
Josue <me@josuebautista.com>
2024-10-03 16:03:34 -0700

    Added Type Chart at top

---
+++ src/TypeChart.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './TypeChart.css'
+
+export default function TypeChart () {
+  return(
+    <div className='block temp-padding-left'>
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/normal.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/fighting.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/flying.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/poison.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/ground.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/rock.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/bug.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/ghost.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/steel.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/fire.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/water.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/grass.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/electric.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/psychic.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/ice.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/dragon.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/dark.png' />
+      <img 
+        className='type-contain'
+        src='https://raw.githubusercontent.com/msikma/pokesprite/refs/heads/master/misc/type-logos/gen8/fairy.png' />
+    </div>
+  )
+}
