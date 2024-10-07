ae4db295bef4b5a6dc64cc1a840f9216948137f5
Josue <me@josuebautista.com>
2024-10-07 09:01:24 -0700

    Starting to add the weakness chart

---
+++ src/WeaknessTable.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './WeaknessTable.css'
+
+export default function WeaknessTable() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Type</th>
+          <td>2x</td>
+          <td>2x</td>
+          <td>2x</td>
+          <td>2x</td>
+          <td>2x</td>
+          <td>2x</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
