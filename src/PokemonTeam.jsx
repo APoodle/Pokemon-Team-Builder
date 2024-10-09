91f760249e90c428d663fc9e801dfa21b6b3a5bb
Josue <me@josuebautista.com>
2024-10-08 19:51:02 -0700

    Currently stuck at showing type weaknesses

--- src/PokemonTeam.jsx
+++ src/PokemonTeam.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRef, useState } from 'react'
 import './PokemonTeam.css'
+import WeaknessTable from './WeaknessTable.jsx'
 
 export default function PokemonTeam() {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
@@ -19,78 +20,81 @@ export default function PokemonTeam() {
   };
 
   return (
-    <div className='flex'>
-      <div>
-        <input 
-          type='text' 
-          placeholder='Search Pokemon'
-          onChange={handleInputChange}
-          ref={searchRef}
-        />
+    <>
+      <div className='flex'>
+        <div>
+          <input 
+            type='text' 
+            placeholder='Search Pokemon'
+            onChange={handleInputChange}
+            ref={searchRef}
+          />
+        </div>
+
+        <div className='team-container flex'>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='0'
+              className='contain' 
+              src={
+                selectedPokemon[0] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[0]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='1'
+              className='contain' 
+              src={
+                selectedPokemon[1] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[1]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='2'
+              className='contain' 
+              src={
+                selectedPokemon[2] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[2]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='3'
+              className='contain' 
+              src={
+                selectedPokemon[3] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[3]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='4'
+              className='contain' 
+              src={
+                selectedPokemon[4] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[4]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+          <div className='pt-border p-2'>
+            <img 
+              onClick={handleClick} 
+              id='5'
+              className='contain' 
+              src={
+                selectedPokemon[5] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[5]}.png`)
+                  : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
+            />
+          </div>
+        </div>
       </div>
-      
-      <div className='team-container flex'>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='0'
-            className='contain' 
-            src={
-              selectedPokemon[0] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[0]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='1'
-            className='contain' 
-            src={
-              selectedPokemon[1] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[1]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='2'
-            className='contain' 
-            src={
-              selectedPokemon[2] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[2]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='3'
-            className='contain' 
-            src={
-              selectedPokemon[3] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[3]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='4'
-            className='contain' 
-            src={
-              selectedPokemon[4] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[4]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-        <div className='pt-border p-2'>
-          <img 
-            onClick={handleClick} 
-            id='5'
-            className='contain' 
-            src={
-              selectedPokemon[5] ? (`https://projectpokemon.org/images/sprites-models/bdsp-sprites/${selectedPokemon[5]}.png`)
-              : ('https://projectpokemon.org/images/sprites-models/bdsp-sprites/465.png')} 
-          />
-        </div>
-      </div>
-    </div>
+      <WeaknessTable pokemonTeam={selectedPokemon} />
+    </>
   )
 }
--- src/WeaknessTable.jsx
+++ src/WeaknessTable.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './WeaknessTable.css'
 
-export default function WeaknessTable() {
+export default function WeaknessTable({ pokemonTeam }) {
+  const [pokemonDetails, setPokemonDetails] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=493')
+    .then(response => response.json())
+    .then(data => {
+      const fetchPokemonDetails = data.results.map(pokemon => fetch(pokemon.url)
+        .then(response => response.json())
+        .then(data => ({
+          name: pokemon.name,
+          id: data.id,
+          type: data.types[0].type.name
+          }))
+        );
+        Promise.all(fetchPokemonDetails).then(pokemonData => {setPokemonDetails(pokemonData)});
+      });
+  }, []);
+  
+
+
   return (
     <div className='pt-5'>
       <table>
         <tr>
           <th>Type</th>
-          <td>2x</td>
-          <td>2x</td>
+          <td>{pokemonTeam[0]}</td>
+          <td>{pokemonTeam[1]}</td>
           <td>2x</td>
           <td>2x</td>
           <td>2x</td>
